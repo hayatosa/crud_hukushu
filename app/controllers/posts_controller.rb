3e31f5ee30f28ab6b9702753c12eb6491727ada1
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2020-11-28 11:07:27 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,13 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    # 投稿詳細ページ(show)へ飛ぶ
+    redirect_to post
   end
 
   def edit
@@ -18,5 +22,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
