6ae18c00aa526dde7f9e405a5074c1dc93888a03
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2020-11-28 10:57:44 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
+  root "posts#index"
+  resources :posts
   resources :users
 end
